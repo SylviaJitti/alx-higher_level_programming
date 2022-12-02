ac70060a3b864f907c39cd1affd86687ab7d9a9d
Sylvia Jitti <72154164+SylviaJitti@users.noreply.github.com>
2022-12-02 16:15:40 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for a in range(ord('a'), ord('z') + 1):
-        if chr(a) != 'e' and chr(a) != 'q':
-                    print("{:c}".format(a), end='')
+for i in range(ord('a'), ord('z') + 1):
+        if i == ord('q') or i == ord('e'):
+                continue
+        print("{0:c}".format(i), end="")
+# ord receives a char and return asci value integer
