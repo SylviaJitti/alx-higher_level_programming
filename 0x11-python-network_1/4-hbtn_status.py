1893dc5c910f4590706764d50348915db360003d
Sylvia Jitti <sylviajitti@gmail.com>
2023-03-31 07:32:52 -0700

    mod5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-"""
-import requests
-
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
+"""Fetches https://alx-intranet.hbtn.io/status"""
+if __name__ == "__main__":
+    import requests
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    response = req.text
     print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
