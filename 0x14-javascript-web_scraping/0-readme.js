bcb132786368de64efa15e9682186311a08ad331
Sylvia Jitti <72154164+SylviaJitti@users.noreply.github.com>
2023-04-26 04:05:29 +0100

    Reading a file in JavaScript

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
-/*This is a script to read a file*/
-
+const process = require('process');
 const fs = require('fs');
 
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
+// The first argument is the file path
+const file = process.argv[2];
+// The content of the file must be written in utf-8
+fs.readFile(file, 'utf8', function (err, data) {
+  // If an error occurred during the reading, print the error object
   if (err) {
-    console.error(err);
-    return;
+    console.log(err);
+  } else {
+    process.stdout.write(data);
   }
-
-  console.log(data);
 });
